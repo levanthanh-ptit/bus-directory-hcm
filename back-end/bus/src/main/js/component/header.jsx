48b3742c2615aae88ffab15b13c109a2bfe9430e
tailouis <vuductaiptit@gmail.com>
2018-10-04 23:25:34 +0700

    update header

--- back-end/bus/src/main/js/component/header.jsx
+++ back-end/bus/src/main/js/component/header.jsx
@@ -2,24 +2,21 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const Header = (props) => {
 
-  render() {
-    return (
-      <div className="content__header">
-        <nav className="content__menu">
-            <ul >
-              <li className="active"><a href="" className="">Trang chủ</a></li>
-              <li className=""><a href="" className="">Tuyến</a></li>
-              <li className=""><a href="" className="">Ban do</a></li>
-              <li className=""><a href="" className="">Tìm đường</a></li>
-            </ul>
-        </nav>
-      </div>
-    )
-  }
+  return (
+    <div className="content__header">
+      <p class="content__header__title">Phan mem ho tro tim tuyen xe bus</p>
+
+      <nav className="content__menu">
+        <ul >
+          <li className="active"><a href="" className="">Trang chủ</a></li>
+          <li className=""><a href="" className="">Tuyến</a></li>
+          <li className=""><a href="" className="">Ban do</a></li>
+          <li className=""><a href="" className="">Tìm đường</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 export { Header };
