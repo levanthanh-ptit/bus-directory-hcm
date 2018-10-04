add7d1043b28cb5aa36266eb7302f61aeda85411
tailouis <vuductaiptit@gmail.com>
2018-10-04 10:07:35 +0700

    config build tool for React

--- back-end/bus/fuse.js
+++ back-end/bus/fuse.js
@@ -1,55 +1,58 @@
 const {
-    FuseBox,
-    SassPlugin,
-    CSSPlugin,
-    QuantumPlugin,
-    CSSResourcePlugin,
-    ImageBase64Plugin,
-    PostCSSPlugin,
-    JSONPlugin
+  FuseBox,
+  SassPlugin,
+  CSSPlugin,
+  QuantumPlugin,
+  CSSResourcePlugin,
+  ImageBase64Plugin,
+  PostCSSPlugin,
+  JSONPlugin
 } = require('fuse-box')
 
 const development = process.env.NODE_ENV !== 'production' &&
-    !process.argv.includes('--production')
+  !process.argv.includes('--production')
 
 const fuse = FuseBox.init({
-    homeDir: 'src',
-    target: 'browser@es5',
-    output: 'public/$name.js',
-    useTypescriptCompiler: true,
-    plugins: [
-        [
-            SassPlugin({
-                importer: true
-            }),
-            CSSResourcePlugin({
-                dist: "public/img",
-                resolve: (f) => `/static/img/${f}`
-            }),
-            development ? CSSPlugin() : CSSPlugin({
-                outFile: _ => 'public/app.css',
-            }),
+  homeDir: 'src',
+  target: 'browser@es5',
+  output: 'public/$name.js',
+  useTypescriptCompiler: true,
+  plugins: [
+    [
+      SassPlugin({
+        importer: true
+      }),
+      CSSResourcePlugin({
+        dist: "public/img",
+        resolve: (f) => `/static/img/${f}`
+      }),
+      development ? CSSPlugin() : CSSPlugin({
+        outFile: _ => 'public/app.css',
+      }),
 
-        ],
-        !development && QuantumPlugin({
-            uglify: true,
-            css: {
-                clean: true
-            },
-            bakeApiIntoBundle: true
-        }),
-        ImageBase64Plugin(), JSONPlugin()
     ],
+    !development && QuantumPlugin({
+      uglify: true,
+      css: {
+        clean: true
+      },
+      bakeApiIntoBundle: true
+    }),
+    ImageBase64Plugin(), JSONPlugin()
+  ],
 })
 
 const bundle = fuse.bundle('app')
-    .instructions('> main/js/index.js')
+  .instructions('> main/js/index.jsx')
 
 if (development) {
-    bundle.hmr({
-        reload: true
-    }).watch()
-    fuse.dev()
+  bundle.hmr({
+    reload: true
+  }).watch()
+  fuse.dev({
+    port: 4444,
+    fallback: 'index.html'
+  })
 }
 
 fuse.run()
---
+++ back-end/bus/src/main/js/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <p>Bus</p>
+      </div>    
+    )
+  }
+}
+
+export {
+  App
+}
