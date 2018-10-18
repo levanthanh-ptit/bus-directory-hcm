4491f7d159ae2b641bd395a203b9c8481cc2d2d4
tailouis <vuductaiptit@gmail.com>
2018-10-18 08:29:09 +0700

    update home css

--- front-end/src/Container/BusHome.jsx
+++ front-end/src/Container/BusHome.jsx
@@ -6,7 +6,16 @@ class BusHome extends React.Component {
         return (
             <div className="bus_home_container" >
               <img src="http://buyttphcm.com.vn/Portals/0/EasyDNNRotator/5616/ii5bzd2i.jpg" alt="none"/>
+                <div>
+                    <div className="bus_home_container__left">
+                        a
+                    </div> 
+                    <div className="bus_home_container__right">
+                       b 
+                    </div>   
+                </div>  
             </div>
+            
         );
     }
 }
